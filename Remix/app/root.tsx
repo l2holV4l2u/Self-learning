4ee5c83c637d0569fd860789a13701a3bfd565a1
l2ho.lV4l2u <59469930+l2holV4l2u@users.noreply.github.com>
2024-01-02 15:11:16 +0700

    Update

--- Remix/app/root.tsx
+++ Remix/app/root.tsx
@@ -1,13 +1,33 @@
+import { json } from "@remix-run/node";
+
 import {
   Form,
   Links,
   LiveReload,
   Meta,
+  Outlet,
+  Link,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css"
+
+import { getContacts } from "./data";
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
+  const { contacts } = useLoaderData();
+  
   return (
     <html lang="en">
       <head>
@@ -35,15 +55,34 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+           {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
